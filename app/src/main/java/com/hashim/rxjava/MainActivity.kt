7570e9108d88f243f84c293f377e20a9ea6bc5e8
hashim <hashimtahir@live.com>
2021-04-18 15:21:29 +0500

    Disposing the Observables.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/rxjava/MainActivity.kt
+++ app/src/main/java/com/hashim/rxjava/MainActivity.kt
@@ -7,10 +7,12 @@ package com.hashim.rxjava
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.hashim.rxjava.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     lateinit var hActivityMainBinding: ActivityMainBinding
+    private val hCompositeDisposable = CompositeDisposable()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         hActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,9 +20,12 @@ class MainActivity : AppCompatActivity() {
 
         /*The object inside the subscribe method is observer
         * this is what recieves the values from observables
+        *
+        * When Observers are no longer required, they should be disposed of
+        * For this, CompositeDisposable is used
         * */
 
-        hGetObserveable()?.subscribe({
+        val hObservalbeDisposable = hGetObserveable()?.subscribe({
             Timber.d("Observable Emitted ${it.hName}")
         }, {
 
@@ -30,26 +35,45 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-        hGetSingle()?.subscribe({
+        val hSingeDisposable = hGetSingle()?.subscribe({
             Timber.d("Single Whole list emitted as Single $it")
         }, {
             Timber.d("Single exception ${it.message}")
         })
 
-        hGetCompletable()?.subscribe({
+        val hCompletableDisposable = hGetCompletable()?.subscribe({
             Timber.d("Completable On Success")
         }, {
             Timber.d("Completable Excetion ${it.message}")
 
         })
 
-        hGetFlowable()?.subscribe({
+        val hFlowableDisposable = hGetFlowable()?.subscribe({
             Timber.d("Flowable $it")
         }, {
             Timber.d("Flowable exception ${it.message}")
         }, {
             Timber.d("Flowable On Complete")
         })
+
+        hCompositeDisposable.addAll(
+            hObservalbeDisposable,
+            hCompletableDisposable,
+            hFlowableDisposable,
+            hSingeDisposable
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        /*Clear cleans all the Subscribers of the observers without disabling the observalbe
+        * they can be subscibed to again if one wants to
+        *
+        * Dispose will do a hard clean. and observables can no longer be subscribed to.
+        * the CompositeDisposible container will no longer be available.
+        * */
+        hCompositeDisposable.clear()
+        hCompositeDisposable.dispose()
 
     }
 
